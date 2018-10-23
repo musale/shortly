a1c8de5cde9b667cae4437cf88295e62f7221027
Martin Musale <martinmshale@gmail.com>
2018-10-23 22:44:31 +0300

    Add changes to initialze the template path and render templates

--- shortly.py
+++ shortly.py
@@ -8,10 +8,20 @@ from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
 
+
 class Shortly(object):
 
     def __init__(self, config):
+        """Initialize redis and render templates."""
         self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+        template_path = os.path.join(os.path.dirname(__file__), 'templates')
+        self.jinja_env = Environment(loader=FileSystemLoader(template_path),
+                                     autoescape=True)
+
+    def render_template(self, template_name, **context):
+        """Render the template."""
+        t = self.jinja_env.get_template(template_name)
+        return Response(t.render(context), mimetype='text/html')
 
     def dispatch_request(self, request):
         return Response('Hello World!')
